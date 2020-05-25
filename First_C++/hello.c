d5cdb74aa1f1b27a183dc8637d8f177b0d2d4ad8
Prateek Singh <prateeksingh@Prateeks-MacBook-Pro.local>
2020-05-25 17:44:08 +0530

    First_Commit_Prateek

---
+++ First_C++/hello.c
@@ -0,0 +1,8 @@
+#include <>
+
+int main()
+{
+   printf("Hello World");
+    return 0;
+
+}
---
+++ First_C++/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Rough day";
+    return 0;
+}
